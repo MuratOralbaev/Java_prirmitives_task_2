408bf43a52fdea138bd50714c716b740309ffc8c
Murat Oralbaev <successmanonly@gmail.com>
2023-03-21 17:05:11 +0300

    Main changed

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,16 +1,16 @@
-public class Main  {
+public class Main {
 
     public static void main(String[] args) {
         int count = 1000;
         int amountDeposit = 2300;
-        int bonus = amountDeposit/100;
+        int bonus = amountDeposit / 100;
         int totalAmount = count + amountDeposit + bonus;
-        if (amountDeposit<1000) {
+        if (amountDeposit < 1000) {
             bonus = 0;
         } else {
             bonus = 1;
         }
-        System.out.println(totalAmount);
+        System.out.println("Ваш баланс: " + totalAmount);
 
 
     }
