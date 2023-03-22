b448f28b846f321abb64887f0bcda32deb2e90d7
Murat Oralbaev <successmanonly@gmail.com>
2023-03-22 12:34:19 +0300

    Fixed Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,14 +3,16 @@ public class Main {
     public static void main(String[] args) {
         int count = 1000;
         int amountDeposit = 2300;
-        int bonus = amountDeposit / 100;
+        int bonus = amountDeposit > 1000 ? amountDeposit / 100 : amountDeposit;
         int totalAmount = count + amountDeposit + bonus;
-        if (amountDeposit < 1000) {
-            bonus = 0;
-        } else {
-            bonus = 1;
-        }
+
+        //if (amountDeposit < 1000) {
+        //bonus = 0;
+        //} else {
+        //bonus = 1;
+        //}
         System.out.println("Ваш баланс: " + totalAmount);
+        System.out.println("Ваши бонусы: " + bonus);
 
 
     }
